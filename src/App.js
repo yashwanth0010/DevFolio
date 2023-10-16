85b28e2419ff4dc0f7cf3ca3079bcf95f7ccef02
yashwant-123 <yashwanthkumar.chamakura@gmail.com>
2023-10-16 22:20:29 +0530

    build folder

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import './App.css';
-import Contact from './Components/Contact';
-import Header from './Components/Header';
-import Hero from './Components/Hero';
-import ProjectDetails from './Components/ProjectDetails';
-import Projects from './Components/Projects';
-import Error404 from './Components/Error';
+import "./App.css";
+import Contact from "./Components/Contact";
+import Header from "./Components/Header";
+import Hero from "./Components/Hero";
+import ProjectDetails from "./Components/ProjectDetails";
+import Projects from "./Components/Projects";
+import Error404 from "./Components/Error";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from './Components/About';
-import Education from './Components/Education';
-import Achievements from './Components/Achievements';
+import About from "./Components/About";
+import Education from "./Components/Education";
+import Achievements from "./Components/Achievements";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    
-    <Routes>
-    <Route path="/" element=
-        {  <>   
-          <Header/>
-          <Hero/>
-          <About/>
-          <Education/>
-          <Projects/>
-          <Achievements/>
-          <Contact/>
-          </> 
-          } 
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Hero />
+                <About />
+                <Education />
+                <Projects />
+                <Achievements />
+                <Contact />
+              </>
+            }
           />
-        <Route path="projects/:id" element={<ProjectDetails />} />
-        <Route path="*" element={<Error404/>} />
-
-      </Routes>
+          <Route path="projects/:id" element={<ProjectDetails />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,38 +1,72 @@
-import React, { Component } from 'react';
-import '../Assets/css/Header.css';
-
+import React, { Component } from "react";
+import "../Assets/css/Header.css";
 
 function Header() {
+  const scrollTo = (ele) => {
+    document
+      .getElementById(ele)
+      ?.scrollIntoView({ block: "start", behavior: "smooth" });
+  };
 
-
-    const scrollTo = (ele) =>{
-      document.getElementById(ele)?.scrollIntoView({ block: "start", behavior: 'smooth' });
-    }
-
-    return (  
+  return (
     <>
-          <header id="header" className="fixed-top">
-    <div className="container d-flex align-items-center justify-content-between">
-
-      <h1 className="logo"><a href="">Portfolio</a></h1>
-      {/*<a href="index.html" className="logo"><img src="assets/img/logo.png" alt="" className="img-fluid"></a>*/}
-
-      <nav id="navbar" className="navbar">
-        <ul>
-            <li onClick={()=>{scrollTo('hero')}}><a className="nav-link scrollto active">Home</a></li>
-            <li onClick={()=>{scrollTo('about')}} ><a className="nav-link scrollto">About</a></li>
-            <li onClick={()=>{scrollTo('education')}} ><a className="nav-link scrollto">Education</a></li>
-            <li onClick={ ()=>{ scrollTo('projects') }}><a className="nav-link scrollto ">Projects </a></li>
-            <li onClick={()=>{scrollTo('achievements')}} ><a className="nav-link scrollto">Achievements</a></li>
-            <li onClick={ ()=>{ scrollTo('contact') }}><a className="nav-link scrollto" >Contact</a></li>
-        </ul>
-        <i className="bi bi-list mobile-nav-toggle"></i>
-      </nav>
-
-    </div>
-  </header>
+      <header id="header" className="fixed-top">
+        <div className="container d-flex align-items-center justify-content-between">
+          <h1 className="logo">
+            <a href="">Portfolio</a>
+          </h1>
+        
+          <nav id="navbar" className="navbar">
+            <ul>
+              <li
+                onClick={() => {
+                  scrollTo("hero");
+                }}
+              >
+                <a className="nav-link scrollto active">Home</a>
+              </li>
+              <li
+                onClick={() => {
+                  scrollTo("about");
+                }}
+              >
+                <a className="nav-link scrollto">About</a>
+              </li>
+              <li
+                onClick={() => {
+                  scrollTo("education");
+                }}
+              >
+                <a className="nav-link scrollto">Education</a>
+              </li>
+              <li
+                onClick={() => {
+                  scrollTo("projects");
+                }}
+              >
+                <a className="nav-link scrollto ">Projects </a>
+              </li>
+              <li
+                onClick={() => {
+                  scrollTo("achievements");
+                }}
+              >
+                <a className="nav-link scrollto">Achievements</a>
+              </li>
+              <li
+                onClick={() => {
+                  scrollTo("contact");
+                }}
+              >
+                <a className="nav-link scrollto">Contact</a>
+              </li>
+            </ul>
+            <i className="bi bi-list mobile-nav-toggle"></i>
+          </nav>
+        </div>
+      </header>
     </>
-    );
+  );
 }
 
 export default Header;
