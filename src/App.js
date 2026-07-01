6eac11d2c740397ff90bb25a66a540226136bbc9
yashwanth0010 <yashwanthkumar.chamakura@gmail.com>
2026-07-01 18:38:31 +0530

    chore: updated emailJS and fixed email trigerring

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import "./App.css";
-import Contact from "./Components/Contact";
-import Header from "./Components/Header";
-import Hero from "./Components/Hero";
-import ProjectDetails from "./Components/ProjectDetails";
-import Projects from "./Components/Projects";
-import Error404 from "./Components/Error";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./Components/About";
-import Education from "./Components/Education";
-import Achievements from "./Components/Achievements";
-import { useRef,useState,useMemo,useEffect } from "react";
-
+import './App.css';
+import Contact from './Components/Contact';
+import Header from './Components/Header';
+import Hero from './Components/Hero';
+import ProjectDetails from './Components/ProjectDetails';
+import Projects from './Components/Projects';
+import Error404 from './Components/Error';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './Components/About';
+import Education from './Components/Education';
+import Achievements from './Components/Achievements';
 
 function App() {
- 
   return (
     <>
       <BrowserRouter>
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,84 +1,123 @@
-import React, { Component,useRef } from 'react';
+import React from 'react';
 import emailjs from '@emailjs/browser';
-import PageAnimation from './PageAnimation';
 
 function Contact() {
-
-
   const sendEmail = (e) => {
     e.preventDefault();
+    const serviceId = process.env.REACT_APP_Email_Service_Id;
+    const templateId = process.env.REACT_APP_Email_Template_Id;
+    const publicKey = process.env.REACT_APP_Email_Public_Key;
+    const form = document.getElementsByTagName('form');
 
-    const form = document.getElementsByTagName("form");
-
-    emailjs.sendForm('service_pe27nlq', 'template_5h3j44m', form['0'], '7ZLFI05xklc_jlD5z')
-    .then((result) => {
-        window.alert("Message Sent Successfully");
+    // Initialize EmailJS with the public key
+    emailjs.init(publicKey);
+    emailjs.sendForm(serviceId, templateId, form['0']).then(
+      (result) => {
+        window.alert('Message Sent Successfully');
         form['0'].reset();
-    }, (error) => {
+      },
+      (error) => {
         console.log(error.text);
-        window.alert("Error In Sending Message");
-    });
+        window.alert('Error In Sending Message');
+      },
+    );
   };
 
-    return (  
+  return (
     <>
-    <section id="contact" className="paralax-mf footer-paralax bg-image sect-mt4 route">
-      <div className="overlay-mf"></div>
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-12">
-            <div className="contact-mf">
-              <div id="contact" className="box-shadow-full" style={{borderRadius :"5rem"}}>
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="title-box-2">
-                      <h5 className="title-left">
-                        Message Me!
-                      </h5>
-                    </div>
-                    <div>
-                      <form onSubmit={sendEmail}   method="post" role="form" className="php-email-form">
-                        <div className="row">
-                          <div className="col-md-12 mb-3">
-                            <div className="form-group">
-                              <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
+      <section
+        id="contact"
+        className="paralax-mf footer-paralax bg-image sect-mt4 route">
+        <div className="overlay-mf"></div>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="contact-mf">
+                <div
+                  id="contact"
+                  className="box-shadow-full"
+                  style={{ borderRadius: '5rem' }}>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <div className="title-box-2">
+                        <h5 className="title-left">Message Me!</h5>
+                      </div>
+                      <div>
+                        <form
+                          onSubmit={sendEmail}
+                          method="post"
+                          role="form"
+                          className="php-email-form">
+                          <div className="row">
+                            <div className="col-md-12 mb-3">
+                              <div className="form-group">
+                                <input
+                                  type="text"
+                                  name="name"
+                                  className="form-control"
+                                  id="name"
+                                  placeholder="Your Name"
+                                  required
+                                />
+                              </div>
                             </div>
-                          </div>
-                          <div className="col-md-12 mb-3">
-                            <div className="form-group">
-                              <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
+                            <div className="col-md-12 mb-3">
+                              <div className="form-group">
+                                <input
+                                  type="email"
+                                  className="form-control"
+                                  name="email"
+                                  id="email"
+                                  placeholder="Your Email"
+                                  required
+                                />
+                              </div>
                             </div>
-                          </div>
-                          <div className="col-md-12 mb-3">
-                            <div className="form-group">
-                              <input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" required />
+                            <div className="col-md-12 mb-3">
+                              <div className="form-group">
+                                <input
+                                  type="text"
+                                  className="form-control"
+                                  name="subject"
+                                  id="subject"
+                                  placeholder="Subject"
+                                  required
+                                />
+                              </div>
                             </div>
-                          </div>
-                          <div className="col-md-12">
-                            <div className="form-group">
-                              <textarea  id="message" className="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+                            <div className="col-md-12">
+                              <div className="form-group">
+                                <textarea
+                                  id="message"
+                                  className="form-control"
+                                  name="message"
+                                  rows="5"
+                                  placeholder="Message"
+                                  required></textarea>
+                              </div>
                             </div>
-                          </div>
-                          <div className="col-md-12 text-center my-3">
-                            {/*<div className="loading">Loading</div>
+                            <div className="col-md-12 text-center my-3">
+                              {/*<div className="loading">Loading</div>
                             <div className="error-message"></div>
     <div className="sent-message">Your message has been sent. Thank you!</div>*/}
+                            </div>
+                            <div className="col-md-12 text-center">
+                              <button
+                                type="submit"
+                                className="button button-a button-big button-rouded">
+                                Send Message
+                              </button>
+                            </div>
                           </div>
-                          <div className="col-md-12 text-center">
-                            <button type="submit" className="button button-a button-big button-rouded">Send Message</button>
-                          </div>
-                        </div>
-                      </form>
+                        </form>
+                      </div>
                     </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="title-box-2 pt-4 pt-md-0">
-                      <h5 className="title-left">
-                        Get in Touch
-                      </h5>
-                    </div>
-                    <div className="more-info">
-                   {/*   
+                    <div className="col-md-6">
+                      <div className="title-box-2 pt-4 pt-md-0">
+                        <h5 className="title-left">Get in Touch</h5>
+                      </div>
+                      <div className="more-info">
+                        {/*   
                    <p className="lead">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolorum dolorem soluta quidem
                         expedita aperiam aliquid at.
@@ -86,18 +125,63 @@ function Contact() {
                         mollitia inventore?
                     </p>   
                     */}
-                      <ul className="list-ico">
-                        <li><span className="bi bi-phone"></span>(+91) 7799995058</li>
-                        <li><span className="bi bi-envelope"></span> <a href="mailto:yashwanthkumar.chamakura@gmail.com">yashwanthkumar.chamakura@gmail.com </a> </li>
-                      </ul>
-                    </div>
-                    <div className="socials">
-                      <ul>
-                      <li><a href="https://www.linkedin.com/in/yashwanth0010/" target="_blank" rel="noopener noreferrer"><span className="ico-circle"><i className="bi bi-linkedin"></i></span></a></li>
-                        <li><a href="https://github.com/yashwanth0010" target="_blank" rel="noopener noreferrer"><span className="ico-circle"><i className="bi bi-github"></i></span></a></li>
-                        <li><a href="https://www.instagram.com/yashwanth0210/" target="_blank" rel="noopener noreferrer"><span className="ico-circle"><i className="bi bi-instagram"></i></span></a></li>
-                        <li><a href="https://twitter.com/yash_1002" target="_blank" rel="noopener noreferrer"><span className="ico-circle"><i className="bi bi-twitter"></i></span></a></li>
-                      </ul>
+                        <ul className="list-ico">
+                          <li>
+                            <span className="bi bi-phone"></span>(+91)
+                            7799995058
+                          </li>
+                          <li>
+                            <span className="bi bi-envelope"></span>{' '}
+                            <a href="mailto:yashwanthkumar.chamakura@gmail.com">
+                              yashwanthkumar.chamakura@gmail.com{' '}
+                            </a>{' '}
+                          </li>
+                        </ul>
+                      </div>
+                      <div className="socials">
+                        <ul>
+                          <li>
+                            <a
+                              href="https://www.linkedin.com/in/yashwanth0010/"
+                              target="_blank"
+                              rel="noopener noreferrer">
+                              <span className="ico-circle">
+                                <i className="bi bi-linkedin"></i>
+                              </span>
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              href="https://github.com/yashwanth0010"
+                              target="_blank"
+                              rel="noopener noreferrer">
+                              <span className="ico-circle">
+                                <i className="bi bi-github"></i>
+                              </span>
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              href="https://www.instagram.com/yashwanth0210/"
+                              target="_blank"
+                              rel="noopener noreferrer">
+                              <span className="ico-circle">
+                                <i className="bi bi-instagram"></i>
+                              </span>
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              href="https://twitter.com/yash_1002"
+                              target="_blank"
+                              rel="noopener noreferrer">
+                              <span className="ico-circle">
+                                <i className="bi bi-twitter"></i>
+                              </span>
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -105,9 +189,9 @@ function Contact() {
             </div>
           </div>
         </div>
-      </div>
-    </section>
-    </>);
+      </section>
+    </>
+  );
 }
 
 export default Contact;
