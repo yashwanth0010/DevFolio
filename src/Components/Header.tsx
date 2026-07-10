1244d2ba0ed688a85fb2cd7a74b61e2d0b29efdb
yashwanth0010 <yashwanthkumar.chamakura@gmail.com>
2026-07-10 23:01:39 +0530

    feat: fixed NavBar scrolling by using react-scroll library

---
+++ src/Components/Header.tsx
@@ -0,0 +1,46 @@
+import "../Assets/css/Header.css";
+import { Link } from 'react-scroll';
+
+function Header() {
+  return (
+    <>
+      <header id="header" className="fixed-top">
+        <div className="container d-flex align-items-center justify-content-between">
+          <h1 className="logo">
+            <a href="">Portfolio</a>
+          </h1>
+
+          <nav id="navbar" className="navbar">
+            <ul>
+              <li
+              >
+                <Link to="hero" smooth={true} duration={100}>Home</Link>
+              </li>
+              <li>
+                <Link to="about" smooth={true} duration={100}>About</Link>
+              </li>
+              <li>
+                <Link to="experience" smooth={true} duration={100}>Experience</Link>
+              </li>
+              <li>
+                <Link to="education" smooth={true} duration={100}>Education</Link>
+              </li>
+              <li>
+                <Link to="projects" smooth={true} duration={100}>Projects</Link>
+              </li>
+              <li>
+                <Link to="achievements" smooth={true} duration={100}>Achievements</Link>
+              </li>
+              <li>
+                <Link to="contact" smooth={true} duration={100}>Contact</Link>
+              </li>
+            </ul>
+            <i className="bi bi-list mobile-nav-toggle"></i>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
